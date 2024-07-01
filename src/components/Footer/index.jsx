593e5b34f9dc7d8aadb8bf3ff05a00edbe30a170
Vitor <vitorsouzasilva121@gmail.com>
2024-07-01 10:56:53 -0300

    add smoother slider and social on footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,9 +1,36 @@
-import styles from "./Footer.module.css"
+import styles from "./Footer.module.css";
+import { useState } from "react";
 
-export default function Footer(){
-    return <footer className={styles.Footer}>
-    <div className={styles.container}>
+function handleInstragram() {
+  window.open("https://www.instagram.com/samzaksistemaseguranca?igsh=OHhzY3U0cnoycHp5");
+}
+
+export default function Footer() {
+  const telNumber = "(11)99649-5733";
+  const [message, setMessage] = useState("");
+
+  function handleCel() {
+    navigator.clipboard.writeText("(11)99649-5733").then(() => {
+      setMessage("Copiado");
+      setTimeout(() => {
+        setMessage("(11)99649-5733");
+      }, 3000); 
+    });
+  }
+
+  return (
+    <footer className={styles.Footer}>
+      <div className={styles.iconsFooter}>
+        <div onClick={handleInstragram} className={styles.iconItem}>
+          <img className={styles.iconsImg} src="src/assets/instagram-footer.png" alt="instagram" />
+          <p>Instagram</p>
+        </div>
+        <div onClick={handleCel} className={styles.iconItem}>
+          <img className={styles.iconsImg} src="src/assets/celular-footer.png" alt="celular" />
+          <p>{message || telNumber}</p>
+        </div>
+      </div>
       <p>&copy; {new Date().getFullYear()} Samzak. Todos os direitos reservados.</p>
-    </div>
-  </footer>
+    </footer>
+  );
 }
--- src/components/Slider/index.jsx
+++ src/components/Slider/index.jsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import styles from "./Slider.module.css";
 import setaDireita from "../../assets/seta-direita.png";
 import setaEsquerda from "../../assets/seta-esquerda.png";
 
 export default function Slider({ images, captions }) {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const imageContainerRef = useRef(null);
 
   const goToPrevious = () => {
     setCurrentImageIndex((prevIndex) =>
@@ -18,20 +19,30 @@ export default function Slider({ images, captions }) {
     );
   };
 
+  useEffect(() => {
+    if (imageContainerRef.current) {
+      imageContainerRef.current.style.transform = `translateX(-${currentImageIndex * 100}%)`;
+    }
+  }, [currentImageIndex]);
+
   return (
     <div className={styles.carrosel}>
-      <button onClick={goToPrevious} className={styles.arrowButton}>
-        <img src={setaEsquerda} alt="Previous" className={styles.arrowRight} />
+      <button onClick={goToPrevious} className={`${styles.arrowButton} ${styles.arrowLeft}`}>
+        <img src={setaEsquerda} alt="Previous" />
       </button>
-      <div className={styles.imageContainer}>
-        <img src={images[currentImageIndex]} className={styles.images} alt="carrosel" />
-        <div className={styles.overlayText}>
-          <p className={styles.title}>{captions[currentImageIndex].title}</p>
-          <p className={styles.subtitle}>{captions[currentImageIndex].subtitle}</p>
-        </div>
+      <div className={styles.imageContainer} ref={imageContainerRef}>
+        {images.map((image, index) => (
+          <div key={index} className={styles.imageSlide}>
+            <img src={image} className={styles.images} alt={`Slide ${index}`} />
+            <div className={styles.overlayText}>
+              <p className={styles.title}>{captions[index].title}</p>
+              <p className={styles.subtitle}>{captions[index].subtitle}</p>
+            </div>
+          </div>
+        ))}
       </div>
-      <button onClick={goToNext} className={styles.arrowButton}>
-        <img src={setaDireita} alt="Next" className={styles.arrowRight} />
+      <button onClick={goToNext} className={`${styles.arrowButton} ${styles.arrowRight}`}>
+        <img src={setaDireita} alt="Next" />
       </button>
     </div>
   );
